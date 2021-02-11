8d7b2e116fb9e70cb4703b668c511e573d480356
Yudi Edri Alviska <yudi.a@weefer.co.id>
2021-02-11 08:51:51 +0700

    Initial Commit

---
+++ MVVMtest/MVVMtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMtestApp.swift
+//  MVVMtest
+//
+//  Created by Yudi Edri Alviska on 11/02/21.
+//
+
+import SwiftUI
+
+@main
+struct MVVMtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
